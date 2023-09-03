61a743acdc2a2ef4559f27a762662c0e40cc8f82
bm16ton <bm16ton@gmail.com>
2023-09-03 00:56:34 -0400

    Add sync to serial, switched the power buttons, way of reporting

--- wz5005-WORKS-needs-prettying/dps.hpp
+++ wz5005-WORKS-needs-prettying/dps.hpp
@@ -9,6 +9,13 @@
 #define MIN_CURRENT 0
 #define MAX_CURRENT 4999
 
+#define BADTXCHKSM  0x90            // 144 bad checksum sent
+#define BADCMNDOROVRFLW 0xA0        // 160 bad command parameter or overflow
+#define BADCMNDCNTEXEC  0xB0        // 176 command cannot be executed
+#define INVALIDCMD    0xC0          // 192 invalid command
+#define UNKNOWNCMD    0xD0          // 208 unknown command
+#define ELSEHRM       0x80          // 128 if it aint one of the ones listed above then it gets this
+
 #define htons2(x) ( ((x)<< 8 & 0xFF00) | ((x)>> 8 & 0x00FF) )
 
 extern volatile int doupdateon;
@@ -19,7 +26,7 @@ struct dps_status {
   uint16_t iset;
   uint16_t uout;
   uint16_t iout;
-  uint16_t power;
+  uint16_t temp;
   uint16_t uin;
   uint16_t lock;
   uint16_t protect;
